fb6ca990e9e4198e5412965ffcaa5fb8e8f81a84
Ig-Matrix <omomaighawosa440@gmal.com>
2022-06-24 19:14:37 +0100

    First commit

---
+++ life.js
@@ -0,0 +1,17 @@
+var num1 = parseInt (prompt('Enter first number: '))
+var opr =  prompt('Enter  Operator: ')
+var num2 = parseInt(prompt('Enter second number: '))
+
+if(opr == "+")
+{
+    console.log(num1 + num2)
+}  else if(opr == '-')
+{
+    console.log(num1 - num2)
+}  else if(opr == '*')
+{
+    console.log(num1 * num2)
+} else if(opr == '/')
+{
+    console.log(num1 / num2)
+}6
